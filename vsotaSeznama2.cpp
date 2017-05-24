af949e4e08010ee9647595f4346e37f232232e70
Nino <nino.basic@famnit.upr.si>
2017-05-24 12:22:27 +0200

    Dodali smo paralelno verzijo programa, ki sešteva elemente seznama.

--- vsotaSeznama2.cpp
+++ vsotaSeznama2.cpp
@@ -9,15 +9,16 @@ using namespace std;
  * Program izračuna vsoto števil.
  */
  
-void vsotaPodseznama(vector<int> &s, int zacetek, int konec, long long &vsota) {
-    for (int i=zacetek; i<konec; i++) {
-       vsota += s[i];
+void vsotaPodseznama(vector<int> &s, int zacetek, int konec, long long &v) {
+    for (int i=zacetek; i<=konec; i++) {
+        v += s[i];
     }
 }
 
 int main() {
     vector<int> seznam;
     int n = 50000000;
+    int m = n / 4;
 
     srand(31337);
     
@@ -27,20 +28,21 @@ int main() {
     }
     
     long long skupnaVsota = 0;
+    long long vsota[4];
+    vector<thread> nit;
     
     clock_t prej = clock(); 
 
-    vector<thread> niti;
-    long long vsota[5];
-    for (int i=0; i<5; i++) {
-        vsota[i] = 0;
-        niti.push_back(thread(vsotaPodseznama, ref(seznam), 10000000*i, 10000000*(i+1), ref(vsota[i])));
-    } 
-
-    for (int i=0; i<5; i++) {
-        niti[i].join();
-        skupnaVsota += vsota[i]; 
+    for (int i=0; i<4; i++) {
+        int konec = m*(i+1) - 1;
+        if (i == 3) konec = n - 1;
+        nit.push_back(thread(vsotaPodseznama, ref(seznam), m*i,  konec, ref(vsota[i])));
     }    
+    
+    for (int i=0; i<4; i++) {
+        nit[i].join();
+        skupnaVsota += vsota[i];
+    }
     
     clock_t potem = clock(); 
 
